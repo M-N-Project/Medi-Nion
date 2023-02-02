bd2e57943dd512a2acf939e266107c98ebc2820e
Gabin Nam <cabin4nam@naver.com>
2023-02-02 12:35:12 +0900

    비즈니스 채널 xml 완성

---
+++ app/src/main/java/com/example/medi_nion/Login.kt
@@ -0,0 +1,12 @@
+package com.example.medi_nion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+}
