ad020922a32e587393db51f557c2b659dda5a4ba
손여산 <mryosan1004@hansung.ac.kr>
2023-02-06 16:32:07 +0900

    병원 프로필 구현중,,

---
+++ app/src/main/java/com/example/medi_nion/HospitalProfile.kt
@@ -0,0 +1,13 @@
+package com.example.medi_nion
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HospitalProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hospital_profile)
+    }
+}
