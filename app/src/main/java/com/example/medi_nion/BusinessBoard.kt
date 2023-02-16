6c7f44361d227d0a76b0c04bf3ca06d0d6e55373
손여산 <mryosan1004@hansung.ac.kr>
2023-02-16 20:18:45 +0900

    왜 안보일까

--- app/src/main/java/com/example/medi_nion/BusinessBoard.kt
+++ app/src/main/java/com/example/medi_nion/BusinessBoard.kt
@@ -4,12 +4,13 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.business_board_items.*
 import kotlinx.android.synthetic.main.business_home.*
 
 class BusinessBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.business_home)
+        setContentView(R.layout.business_board_recom)
 
         val businessBoard : List<BusinessBoardItem> = listOf(
             BusinessBoardItem("title", "2023년 2월 15일 오전 11시 10분",
@@ -19,6 +20,6 @@ class BusinessBoard : AppCompatActivity() {
         )
 
         val adapter = BusinessAdapter(businessBoard)
-        
+        BusinessBoardRecyclerView.adapter = adapter
     }
 }
