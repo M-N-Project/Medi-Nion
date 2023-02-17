a76e28ca2ecc66418c9e6cf085846bdeb8647d21
손여산 <mryosan1004@hansung.ac.kr>
2023-02-17 14:59:18 +0900

    내 비즈니스 관리하기 리사이클러 구현

--- app/src/main/java/com/example/medi_nion/BusinessBoard.kt
+++ app/src/main/java/com/example/medi_nion/BusinessBoard.kt
@@ -1,10 +1,8 @@
 package com.example.medi_nion
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.business_board_items.*
 import kotlinx.android.synthetic.main.business_home.*
 
 class BusinessBoard : AppCompatActivity() {
@@ -12,14 +10,14 @@ class BusinessBoard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.business_board_recom)
 
-        val businessBoard : List<BusinessBoardItem> = listOf(
-            BusinessBoardItem("title", "2023년 2월 15일 오전 11시 10분",
-                BitmapFactory.decodeResource(resources, R.drawable.bell_icon), "이것은 내용입니다", 1, 1),
-            BusinessBoardItem("title1", "2023년 2월 16일 오전 11시 10분",
-                BitmapFactory.decodeResource(resources, R.drawable.logo), "이것은 내용1입니다", 1, 1)
-        )
+//        val businessBoard : List<BusinessBoardItem> = listOf(
+//            BusinessBoardItem("title", "2023년 2월 15일 오전 11시 10분",
+//                BitmapFactory.decodeResource(resources, R.drawable.bell_icon), "이것은 내용입니다", 1, 1),
+//            BusinessBoardItem("title1", "2023년 2월 16일 오전 11시 10분",
+//                BitmapFactory.decodeResource(resources, R.drawable.logo), "이것은 내용1입니다", 1, 1)
+//        )
 
-        val adapter = BusinessAdapter(businessBoard)
-        BusinessBoardRecyclerView.adapter = adapter
+//        val adapter = BusinessRecyclerAdapter(businessBoard)
+//        BusinessBoardRecyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/medi_nion/BusinessManageActivity.kt
+++ app/src/main/java/com/example/medi_nion/BusinessManageActivity.kt
@@ -1,7 +1,7 @@
 package com.example.medi_nion
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -9,18 +9,28 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.business_home.*
 
 class BusinessManageActivity : AppCompatActivity() {
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.business_manage_create)
 
-        val businessBoard : List<BusinessBoardItem> = listOf( //일단 더미데이터, db 연동해야함
-            BusinessBoardItem("title", "2023년 2월 15일 오전 11시 10분",
-                BitmapFactory.decodeResource(resources, R.drawable.bell_icon), "이것은 내용입니다", 1, 1),
-            BusinessBoardItem("title1", "2023년 2월 16일 오전 11시 10분",
-                BitmapFactory.decodeResource(resources, R.drawable.logo), "이것은 내용1입니다", 1, 1)
-        )
+//        val businessBoard : ArrayList<BusinessBoardItem> = listOf(
+//             BusinessBoardItem("title", "2023년 2월 15일 오전 11시 10분",
+//                BitmapFactory.decodeResource(resources, R.drawable.bell_icon), "이것은 내용입니다", 1, 1),
+//            BusinessBoardItem("title1", "2023년 2월 16일 오전 11시 10분",
+//                BitmapFactory.decodeResource(resources, R.drawable.logo), "이것은 내용1입니다", 1, 1)
+//        ) //businessBoard end
+        //일단 더미데이터, db 연동해야함
 
-        val adapter = BusinessAdapter(businessBoard)
+        val businessBoard = ArrayList<BusinessBoardItem>()
+        businessBoard.add(BusinessBoardItem(getDrawable(R.drawable.business_profile_img)!!, "개강전 이벤트!!", "2023년 2월 15일 오후 1시 30분",
+        "이것은 내용입니다. 약사세요~ 줄바꿈도 해야한답니다", 1, 2))
+
+        businessBoard.add(BusinessBoardItem(getDrawable(R.drawable.business_profile_img)!!, "1월 이벤트!!", "2023년 1월 11일 오전 10시 30분",
+            "반가워요 1월이 밝았네요 이벤트 어쩌구 저쩌구", 3, 2))
+
+        //이벤트 연결중,,,
+        val adapter = BusinessRecyclerAdapter(businessBoard)
         BusinessBoardRecyclerView.adapter = adapter
 
         var write = findViewById<Button>(R.id.write_btn)
