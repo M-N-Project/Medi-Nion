d13f91dcc44bc4b593ef774df25522625280c32e
손여산 <mryosan1004@hansung.ac.kr>
2023-02-21 14:22:00 +0900

    비즈니스 프사 잃고 관리창 약간 수정4

---
+++ app/src/main/java/com/example/medi_nion/BusinessProfile.kt
@@ -0,0 +1,106 @@
+package com.example.medi_nion
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.board_home.*
+import kotlinx.android.synthetic.main.business_home.*
+import org.json.JSONException
+import org.json.JSONObject
+
+
+var businessitems = ArrayList<BusinessBoardItem>()
+//val viewModel = BoardViewModel()
+lateinit var businessadapter : BusinessRecyclerAdapter
+lateinit var businessmJsonString1: String
+var businesserrorString: String? = null
+
+class BusinessProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) { //프레그먼트로 생길 문제들은 추후에 생각하기,,
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.board_home)
+
+        val url = "http://seonho.dothome.co.kr/BusinessProfile.php"
+
+
+        fetchData()
+
+        val adapter = BusinessRecyclerAdapter(businessitems)
+        BusinessBoardRecyclerView.adapter = adapter
+
+//        val dataObserver: Observer<ArrayList<BoardItem>> = Observer {
+//            items.value = it
+//            val adapter = BoardListAdapter(items)
+//            boardRecyclerView.adapter = adapter
+//        }
+//
+//        viewModel.itemList.observe(this, dataObserver)
+
+
+//        adapter.setOnItemClickListener(object : BoardListAdapter.OnItemClickListener {
+//            override fun onItemClick(v: View, data: BusinessBoardItem, pos: Int) {
+//                Intent(this@BusinessProfile, BusinessBoard::class.java).apply {
+//                    putExtra("data", data.toString())
+//                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                }.run { startActivity(this) }
+//            }
+//
+//        })
+
+        //글쓰기
+        val write = findViewById<Button>(R.id.write_btn)
+        write.setOnClickListener {
+            val intent = Intent(applicationContext, BusinessWriting::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun fetchData() {
+        // url to post our data
+        val url = "http://seonho.dothome.co.kr/BusinessProfile.php"
+
+        val queue = Volley.newRequestQueue(this@BusinessProfile)
+
+        val request: StringRequest = object : StringRequest(
+            Method.POST, url,
+            Response.Listener { response ->
+                try {
+                    val jsonObject = JSONObject(response)
+                    val title = jsonObject.getString("title")
+                    val content = jsonObject.getString("content")
+                    val boardItem = BoardItem(title, content)
+                    items.add(boardItem)
+                    val adapter = BoardListAdapter(items)
+                    boardRecyclerView.adapter = adapter
+                    Log.d("lllaaa", boardItem.title)
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            }, Response.ErrorListener { error -> // method to handle errors.
+                Log.d("pppppppp", "Fail to get course$error")
+                Toast.makeText(this@BusinessProfile, "Fail to get course$error", Toast.LENGTH_SHORT)
+                    .show()
+            }) {
+            override fun getBodyContentType(): String {
+                // as we are passing data in the form of url encoded
+                // so we are passing the content type below
+                return "application/x-www-form-urlencoded; charset=UTF-8"
+            }
+
+        }
+        queue.add(request)
+
+    }
+
+    data class JsonObj(val result: List<BoardItem>)
+}
