46ef69e0f3eb14d6a06682843390b29d8b085260
손여산 <mryosan1004@hansung.ac.kr>
2023-02-21 15:02:49 +0900

    비즈니스 프사 잃고 관리창 약간 수정5

---
+++ app/src/main/java/com/example/medi_nion/BusinessProfileItem.kt
@@ -0,0 +1,12 @@
+package com.example.medi_nion
+
+import android.graphics.drawable.Drawable
+
+class BusinessProfileItem(
+    val Channel_Name: String,
+    val Channel_Message: String,
+    val id: String,
+    val Channel_Profile_Img: Drawable,
+    val Channel_Back_Img: Drawable
+) {
+}
