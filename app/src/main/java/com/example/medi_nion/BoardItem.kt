ec213997686557f29a46bf4cb022227405f73a83
5lft <zara0140@naver.com>
2023-03-04 10:35:43 +0900

    master랑 머지

--- app/src/main/java/com/example/medi_nion/BoardItem.kt
+++ app/src/main/java/com/example/medi_nion/BoardItem.kt
@@ -5,8 +5,5 @@ import java.sql.Timestamp
 
 class BoardItem(val num: Int, val title: String, val contents: String, val time:String, val image :String) {
 
-    var num:Int = 0
 
-    fun setPostNum(num:Int) {this.num = num}
-    fun getPostNum(): Int {return num}
 }
