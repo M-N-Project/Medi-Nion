ee0266b8d3567e6995cd32fe6524d0e9e5eaa2a3
장선호 <sunho3456@naver.com>
2023-03-08 17:28:57 +0900

    충돌 헤헤

--- app/src/main/java/com/example/medi_nion/BoardDetail.kt
+++ app/src/main/java/com/example/medi_nion/BoardDetail.kt
@@ -37,7 +37,7 @@ val viewModel: CommentViewModel = CommentViewModel()
 class BoardDetail : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) { //프레그먼트로 생길 문제들은 추후에 생각하기,,
         super.onCreate(savedInstanceState)
         setContentView(R.layout.board_detail)
@@ -51,7 +51,7 @@ class BoardDetail : AppCompatActivity() {
         val Book_Btn = findViewById<CheckBox>(R.id.checkbox_bookmark2) //북마크 imageview 부분
         val Book_count = findViewById<TextView>(R.id.textView_bookmarkcount2) //북마크 count 부분
 
-        val manager : InputMethodManager =
+        val manager: InputMethodManager =
             getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
 
@@ -86,7 +86,6 @@ class BoardDetail : AppCompatActivity() {
         val comment_num = findViewById<TextView>(R.id.comment_num)
 
 
-
 //        textView_num.setText(num)
         title_textView.setText(title)
         content_textView.setText(content)
@@ -103,7 +102,7 @@ class BoardDetail : AppCompatActivity() {
         CommentRecyclerView.adapter = Commentadapter
 
         Comment_Btn.setOnClickListener {
-            if(Comment_editText.text.toString().isEmpty()) {
+            if (Comment_editText.text.toString().isEmpty()) {
                 Toast.makeText(baseContext, "댓글을 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
                 CommentRequest()
@@ -112,40 +111,35 @@ class BoardDetail : AppCompatActivity() {
                     InputMethodManager.HIDE_NOT_ALWAYS
                 ) //Comment버튼 누르면 키보드 내리기
                 Comment_editText.setText(null) //댓글입력창 clear
-                comment_count++
-            CommentRequest()
-            manager.hideSoftInputFromWindow(getCurrentFocus()?.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS) //Comment버튼 누르면 키보드 내리기
-            Comment_editText.setText(null) //댓글입력창 clear
 //            comment_num.text = comment_count.toString()
-        }
-
-
-        Like_Btn.setOnClickListener {
-            //좋아요 눌렀을때,,
-
-            //likeRequest()
-
-            isDefault = !isDefault
-
-            if(isDefault) {
-                count--
-                Like_count.text = count.toString() //Like_count를 감소시키기
-                Like_Btn.setImageResource(R.drawable.favorite_border)
             }
-            else {
-                count++
-                Like_count.text = count.toString() //Like_count를 증가시키기
-                Like_Btn.setImageResource(R.drawable.favorite_fill)
-                LikeRequest()
-            }
-        }
 
-        Book_Btn.setOnClickListener {
-            if(Book_Btn.isChecked()) {
-                Book_Create_request()
+
+            Like_Btn.setOnClickListener {
+                //좋아요 눌렀을때,,
+
+                //likeRequest()
+
+                isDefault = !isDefault
+
+                if (isDefault) {
+                    count--
+                    Like_count.text = count.toString() //Like_count를 감소시키기
+                    Like_Btn.setImageResource(R.drawable.favorite_border)
+                } else {
+                    count++
+                    Like_count.text = count.toString() //Like_count를 증가시키기
+                    Like_Btn.setImageResource(R.drawable.favorite_fill)
+                    LikeRequest()
+                }
             }
-            else {
-                Book_Delete_request()
+
+            Book_Btn.setOnClickListener {
+                if (Book_Btn.isChecked()) {
+                    Book_Create_request()
+                } else {
+                    Book_Delete_request()
+                }
             }
         }
     }
@@ -185,19 +179,19 @@ class BoardDetail : AppCompatActivity() {
 //    }
 
 
-
     fun LikeRequest() {  //좋아요 DB연동중
         var id = intent?.getStringExtra("id").toString() //user id 받아오기, 내가 좋아요 한 글 보기 위함
         var num = intent?.getIntExtra("num", 0).toString() //게시물 num id 받아오기, 게시물 좋아요 개수 구분하기 위함
-        var heart = findViewById<ImageView>(R.id.imageView_like2).toString() //좋아요 클릭만 가져오게 하기(익명이라 누가 눌렀는진 의미 없을듯,,)
+        var heart =
+            findViewById<ImageView>(R.id.imageView_like2).toString() //좋아요 클릭만 가져오게 하기(익명이라 누가 눌렀는진 의미 없을듯,,)
         var heart_count = findViewById<TextView>(R.id.textView_likecount2).text.toString()
         val url = "http://seonho.dothome.co.kr/Heart.php"
 
-        val request = Login_Request (
+        val request = Login_Request(
             Request.Method.POST,
             url,
             { response ->
-                if(!response.equals("Like fail")) {
+                if (!response.equals("Like fail")) {
                     heart_count = response.toString()
                     num = response.toString()
 
@@ -229,12 +223,11 @@ class BoardDetail : AppCompatActivity() {
         var post_num = intent?.getIntExtra("num", 0).toString()
         var url = "http://seonho.dothome.co.kr/BookmarkDelete.php"
 
-        val request = Login_Request (
+        val request = Login_Request(
             Request.Method.POST,
             url,
-            {
-                    response ->
-                if(!response.equals("Bookmark fail")) {
+            { response ->
+                if (!response.equals("Bookmark fail")) {
 
                     Toast.makeText(
                         baseContext,
@@ -247,7 +240,7 @@ class BoardDetail : AppCompatActivity() {
                     )
 
                     //fetchBookmarkData()
-                }  else {
+                } else {
 
                     Toast.makeText(
                         applicationContext,
@@ -271,32 +264,31 @@ class BoardDetail : AppCompatActivity() {
         var post_num = intent?.getIntExtra("num", 0).toString()
         var url = "http://seonho.dothome.co.kr/Bookmark.php"
 
-        val request = Login_Request (
+        val request = Login_Request(
             Request.Method.POST,
             url,
-            {
-                response ->
-                    if(!response.equals("Bookmark fail")) {
+            { response ->
+                if (!response.equals("Bookmark fail")) {
 
-                        Toast.makeText(
-                            baseContext,
-                            String.format("북마크가 생성되었습니다."),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        Log.d(
-                            "bookmark success",
-                            "$id, $post_num"
-                        )
+                    Toast.makeText(
+                        baseContext,
+                        String.format("북마크가 생성되었습니다."),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d(
+                        "bookmark success",
+                        "$id, $post_num"
+                    )
 
-                        //fetchBookmarkData()
-                    }  else {
+                    //fetchBookmarkData()
+                } else {
 
-                        Toast.makeText(
-                            applicationContext,
-                            "북마크를 생성할 수 없습니다.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    Toast.makeText(
+                        applicationContext,
+                        "북마크를 생성할 수 없습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }, { Log.d("Comment Failed", "error......${error(applicationContext)}") },
 
             hashMapOf(
@@ -311,25 +303,26 @@ class BoardDetail : AppCompatActivity() {
     fun fetchBookmarkData() {
         val url = "http://seonho.dothome.co.kr/Comment_list.php"
         var post_num = intent?.getIntExtra("num", 0).toString()
-        val jsonArray : JSONArray
+        val jsonArray: JSONArray
 
         val request = Login_Request(
             Request.Method.POST,
             url,
             { response ->
                 Comment_items.clear()
-                if(response != "no Comment"){
+                if (response != "no Comment") {
                     val jsonArray = JSONArray(response)
 
-                    var  comment_user = HashMap<String, Int>()
+                    var comment_user = HashMap<String, Int>()
 
-                    for(i in 0 until jsonArray.length()) {
+                    for (i in 0 until jsonArray.length()) {
                         val item = jsonArray.getJSONObject(i)
                         var id = item.getString("id")
-                        if(!comment_user.containsKey(id)) comment_user[id] = comment_user.size+1
+                        if (!comment_user.containsKey(id)) comment_user[id] =
+                            comment_user.size + 1
                     }
 
-                    for(i in 0 until jsonArray.length()) {
+                    for (i in 0 until jsonArray.length()) {
 
                         val item = jsonArray.getJSONObject(i)
 
@@ -344,22 +337,34 @@ class BoardDetail : AppCompatActivity() {
                         viewModel.setItemList(Comment_items)
 
                         //댓글 아이템 하나 누르면
-                        var manager : InputMethodManager =
+                        var manager: InputMethodManager =
                             getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                         val Comment_editText = findViewById<EditText>(R.id.Comment_editText)
                         val Comment_Btn = findViewById<Button>(R.id.Comment_Btn)
-                        val comment2_linearLayout = findViewById<LinearLayout>(R.id.comment2_linearLayout)
+                        val comment2_linearLayout =
+                            findViewById<LinearLayout>(R.id.comment2_linearLayout)
 
-                        Commentadapter.setOnItemClickListener(object : CommentListAdapter.OnItemClickListener {
+                        Commentadapter.setOnItemClickListener(object :
+                            CommentListAdapter.OnItemClickListener {
                             override fun onItemClick(v: View, data: CommentItem, pos: Int) {
-                                Toast.makeText(applicationContext, String.format("대댓글 ? "), Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    applicationContext,
+                                    String.format("대댓글 ? "),
+                                    Toast.LENGTH_SHORT
+                                ).show()
                                 Comment_editText.requestFocus()
-                                manager.showSoftInput(Comment_editText, WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN) //키보드 올리기
+                                manager.showSoftInput(
+                                    Comment_editText,
+                                    WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
+                                ) //키보드 올리기
 
                                 Comment_Btn.setOnClickListener {
-                                    Toast.makeText(applicationContext, String.format("우왕"), Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(
+                                        applicationContext,
+                                        String.format("우왕"),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                                     //comment2_linearLayout.visibility = View.VISIBLE
-                                    Comment2Request()
                                 }
 
                             }
@@ -375,7 +380,6 @@ class BoardDetail : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this)
         queue.add(request)
     }
-
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -437,28 +441,30 @@ class BoardDetail : AppCompatActivity() {
         val url = "http://seonho.dothome.co.kr/Comment_list.php"
         val urlDetail = "http://seonho.dothome.co.kr/commentInfoDetail.php"
         var post_num = intent?.getIntExtra("num", 0).toString()
-        val jsonArray : JSONArray
+        val jsonArray: JSONArray
 
         val request = Login_Request(
             Request.Method.POST,
             url,
             { response ->
                 Comment_items.clear()
-                if(response != "no Comment"){
+                if (response != "no Comment") {
                     val jsonArray = JSONArray(response)
 
                     val comment_count = jsonArray.length()
-                    findViewById<TextView>(R.id.textView_commentcount2).text = comment_count.toString()
+                    findViewById<TextView>(R.id.textView_commentcount2).text =
+                        comment_count.toString()
 
-                    var  comment_user = HashMap<String, Int>()
+                    var comment_user = HashMap<String, Int>()
 
-                    for(i in 0 until jsonArray.length()) {
+                    for (i in 0 until jsonArray.length()) {
                         val item = jsonArray.getJSONObject(i)
                         var id = item.getString("id")
-                        if(!comment_user.containsKey(id)) comment_user[id] = comment_user.size+1
+                        if (!comment_user.containsKey(id)) comment_user[id] =
+                            comment_user.size + 1
                     }
 
-                    for(i in 0 until jsonArray.length()) {
+                    for (i in 0 until jsonArray.length()) {
                         val item = jsonArray.getJSONObject(i)
 
                         val id = item.getString("id")
@@ -472,13 +478,15 @@ class BoardDetail : AppCompatActivity() {
                         //viewModel.setItemList(Comment_items)
                         CommentRecyclerView.adapter = Commentadapter
 
-                        var detailId : String = ""
-                        var detailComment : String = ""
-                        var detailCommentTime : String = ""
+                        var detailId: String = ""
+                        var detailComment: String = ""
+                        var detailCommentTime: String = ""
 
-                        Commentadapter.setOnItemClickListener(object : CommentListAdapter.OnItemClickListener {
+                        Commentadapter.setOnItemClickListener(object :
+                            CommentListAdapter.OnItemClickListener {
                             override fun onItemClick(v: View, data: CommentItem, pos: Int) {
-                                Toast.makeText(applicationContext, "제발", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(applicationContext, "제발", Toast.LENGTH_SHORT)
+                                    .show()
 
                                 val request = Login_Request(
                                     Request.Method.POST,
@@ -495,16 +503,28 @@ class BoardDetail : AppCompatActivity() {
                                         detailCommentTime = jsonObject.getString("comment_time")
 
 
-                                        val intent = Intent(applicationContext, CommentDetail::class.java)
+                                        val intent = Intent(
+                                            applicationContext,
+                                            CommentDetail::class.java
+                                        )
                                         intent.putExtra("comment_num", data.comment_num)
                                         intent.putExtra("id", id)
                                         intent.putExtra("comment", detailComment)
                                         intent.putExtra("comment_time", detailCommentTime)
 
-                                        Log.d("commentdetail", "${data.comment_num}, $id, $detailComment, $detailCommentTime")
+                                        Log.d(
+                                            "commentdetail",
+                                            "${data.comment_num}, $id, $detailComment, $detailCommentTime"
+                                        )
                                         startActivity(intent)
 
-                                    }, { Log.d("Comment failed", "error......${error(applicationContext)}") },
+                                    },
+                                    {
+                                        Log.d(
+                                            "Comment failed",
+                                            "error......${error(applicationContext)}"
+                                        )
+                                    },
                                     hashMapOf(
                                         "comment_num" to data.comment_num.toString()
                                     )
@@ -529,7 +549,7 @@ class BoardDetail : AppCompatActivity() {
     fun StringToBitmaps(image: String?): Bitmap? {
         try {
             val encodeByte = Base64.decode(image, Base64.DEFAULT)
-            val bitmap : Bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
+            val bitmap: Bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
             return bitmap
         } catch (e: Exception) {
             e.message
