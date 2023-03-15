f6f8c6af0fdaa98ecd3563588473bd9e76169568
장선호 <sunho3456@naver.com>
2023-03-15 17:18:17 +0900

    메뉴 xml 수정 완료

--- app/src/main/java/com/example/medi_nion/MenuFragment.kt
+++ app/src/main/java/com/example/medi_nion/MenuFragment.kt
@@ -2,11 +2,9 @@ package com.example.medi_nion
 
     import android.content.Intent
     import android.os.Bundle
-    import android.util.Log
     import android.view.View
     import android.widget.Button
     import androidx.fragment.app.Fragment
-    import kotlinx.android.synthetic.main.login.*
 
 class MenuFragment : Fragment(R.layout.bottom_menu) { //menu 창으로 이동하는 프레그먼트
 
@@ -28,19 +26,18 @@ class MenuFragment : Fragment(R.layout.bottom_menu) { //menu 창으로 이동하
 
         val id = arguments?.getString("id")
 
-        basicBoard = view.findViewById(R.id.menu1) // 전체 게시판
-        jobBoard = view.findViewById(R.id.menu2) //직종별 게시판
-        secBoard = view.findViewById(R.id.menu3) //진료과별 게시판
-        hosBoard = view.findViewById(R.id.menu4) //우리 병원 게시판
-        dealBoard = view.findViewById(R.id.menu5) // 장터 게시판
+        basicBoard = view.findViewById(R.id.menu_basic) // 전체 게시판
+        jobBoard = view.findViewById(R.id.menu_job) //직종별 게시판
+        secBoard = view.findViewById(R.id.menu_dept) //진료과별 게시판
+        dealBoard = view.findViewById(R.id.menu_market) // 장터 게시판
 
-        qnaBtn = view.findViewById(R.id.menu6)
+        qnaBtn = view.findViewById(R.id.menu_qna)
 
         academyBtn = view.findViewById(R.id.menu7)
         employeeInfoBtn = view.findViewById(R.id.menu8)
         hospitalHomePageBtn = view.findViewById(R.id.menu9)
-        medicalNewsBtn = view.findViewById(R.id.menu10)
-        manageBusinessBtn = view.findViewById(R.id.menu11)
+        medicalNewsBtn = view.findViewById(R.id.menu_news)
+        manageBusinessBtn = view.findViewById(R.id.menu_buss)
 
         employeeInfoBtn.setOnClickListener { //병원 프로필 및 채용 정보로 이동함
             activity?.let{
@@ -70,14 +67,6 @@ class MenuFragment : Fragment(R.layout.bottom_menu) { //menu 창으로 이동하
                 val intent = Intent(context, Board::class.java)
                 intent.putExtra("id", id)
                 intent.putExtra("board", "진료과별 게시판")
-                startActivity(intent)
-            }
-        }
-        hosBoard.setOnClickListener { //게시판으로 이동함
-            activity?.let{
-                val intent = Intent(context, Board::class.java)
-                intent.putExtra("id", id)
-                intent.putExtra("board", "우리 병원 게시판")
                 startActivity(intent)
             }
         }
