49b441efa7649d7155261c2f43058571b7030f76
손여산 <mryosan1004@hansung.ac.kr>
2023-03-23 12:11:59 +0900

    Retrofit2 대공사1

---
+++ app/src/main/java/com/example/medi_nion/Retrofit2_Interface/SignUp_Request.kt
@@ -0,0 +1,19 @@
+package com.example.medi_nion.Retrofit2_Interface
+
+import com.example.medi_nion.Retrofit2_Dataclass.Data_SignUp_Request
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface SignUp_Request {
+    @FormUrlEncoded
+    @POST("SignUP.php")
+    fun getUser(
+        @Field("nickname") nickname: String,
+        @Field("id") id: String,
+        @Field("passwd") passwd :String,
+        @Field("userType") userType :String,
+        @Field("userDept") userDept :String
+    ): Call<Data_SignUp_Request>
+}
