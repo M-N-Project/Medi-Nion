7c2e91947dbb607d19154ab60caa434b62109b1d
Gabin Nam <cabin4nam@naver.com>
2023-03-23 12:49:58 +0900

    댓글 좋아요... 대댓글 좋아요... 하는즁

--- app/src/main/java/com/example/medi_nion/CommentListAdapter.kt
+++ app/src/main/java/com/example/medi_nion/CommentListAdapter.kt
@@ -10,28 +10,31 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CommentListAdapter(private var itemList : ArrayList<CommentItem>) : RecyclerView.Adapter<CommentListAdapter.ViewHolder>()  {
+class CommentListAdapter(private var itemList : ArrayList<CommentItem>) : RecyclerView.Adapter<CommentListAdapter.ViewHolder>() {
 
     var datas = mutableListOf<CommentItem>()
 
-    interface OnItemClickListener{
-        fun onItemClick(v:View, data: CommentItem, pos: Int)
+    interface OnItemClickListener {
+        fun onItemClick(v: View, data: CommentItem, pos: Int)
         fun onItemHeart(v: View, data: CommentItem, pos: Int)
-        fun onItemComment(v: View, data:CommentItem, pos:Int)
+        fun onItemComment(v: View, data: CommentItem, pos: Int)
     }
-    private var listener : OnItemClickListener? = null
 
-    fun setOnItemClickListener(listener : CommentListAdapter.OnItemClickListener) {
+    private var listener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: CommentListAdapter.OnItemClickListener) {
         this.listener = listener
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.board_comment_item, parent, false)
+        val inflatedView =
+            LayoutInflater.from(parent.context).inflate(R.layout.board_comment_item, parent, false)
         return ViewHolder(inflatedView);
     }
 
-    override fun getItemCount(): Int = itemList.size //라이브데이터 사용할때는 datas사용, 지금은 더미 데이터라서 매개변수로 넘긴 itemList로 대체
+    override fun getItemCount(): Int =
+        itemList.size //라이브데이터 사용할때는 datas사용, 지금은 더미 데이터라서 매개변수로 넘긴 itemList로 대체
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(itemList[position], position)
@@ -41,52 +44,54 @@ class CommentListAdapter(private var itemList : ArrayList<CommentItem>) : Recycl
         private val itemComment: TextView = itemView.findViewById(R.id.comment_content)
         private val itemCommentTime: TextView = itemView.findViewById(R.id.comment_time)
         private val itemCommentNum: TextView = itemView.findViewById(R.id.comment_num)
-        private val itemCommentHeart : CheckBox = itemView.findViewById(R.id.imageView_comment_like)
-        private val itemCommentHeartCnt : TextView = itemView.findViewById(R.id.comment_heart_count)
-        private val itemCommentComment: ImageView = itemView.findViewById(R.id.imageView_comment_comment)
-        private val itemCommentDetail: RecyclerView = itemView.findViewById(R.id.CommentRecyclerView2)
+        private val itemCommentHeart: CheckBox = itemView.findViewById(R.id.imageView_comment_like)
+        private val itemCommentHeartCnt: TextView = itemView.findViewById(R.id.comment_heart_count)
+        private val itemCommentComment: ImageView =
+            itemView.findViewById(R.id.imageView_comment_comment)
+        private val itemCommentDetail: RecyclerView =
+            itemView.findViewById(R.id.CommentRecyclerView2)
 
-        fun bind(item: CommentItem, pos : Int) {
+        fun bind(item: CommentItem, pos: Int) {
             itemComment.text = item.comment
             itemCommentTime.text = item.comment_time
             itemCommentNum.text = item.writerNum.toString()
             itemCommentHeartCnt.text = item.commentHeart.toString()
 
-            if(item.isHeart == true) itemCommentHeart.isChecked = true
+            if (item.isHeart == true) itemCommentHeart.isChecked = true
             else itemCommentHeart.isChecked = false
 
 //            if(pos == item.comment_num-1){
 //                itemCommentDetail.adapter = item.commentDetailAdapterMap[pos]
 //            }
-            itemCommentDetail.adapter = item.commentDetailAdapter
+            if (item.commentDetailAdapter != null)
+                itemCommentDetail.adapter = item.commentDetailAdapter
 
 
-            val pos = absoluteAdapterPosition
-            if(pos!= RecyclerView.NO_POSITION)
-            {
-                itemView.setOnClickListener {
-                    listener?.onItemClick(itemView,item,pos)
-                    Log.d("ItemClick1", "Event1")
+                val pos = absoluteAdapterPosition
+                if (pos != RecyclerView.NO_POSITION) {
+                    itemView.setOnClickListener {
+                        listener?.onItemClick(itemView, item, pos)
+                        Log.d("ItemClick1", "Event1")
+                    }
+                    itemComment.setOnClickListener {
+                        listener?.onItemClick(itemView, item, pos)
+                    }
+
+                    itemCommentHeart.setOnClickListener {
+                        listener?.onItemHeart(itemView, item, pos)
+                    }
+
+                    itemCommentComment.setOnClickListener {
+                        listener?.onItemComment(itemView, item, pos)
+                    }
+
                 }
-                itemComment.setOnClickListener {
-                    listener?.onItemClick(itemView,item,pos)
-                }
-
-                itemCommentHeart.setOnClickListener{
-                    listener?.onItemHeart(itemView,item,pos)
-                }
-
-                itemCommentComment.setOnClickListener{
-                    listener?.onItemComment(itemView,item,pos)
-                }
-
             }
         }
-    }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun setData(newData:ArrayList<CommentItem>){
-        itemList = newData
-        notifyDataSetChanged()
+        @SuppressLint("NotifyDataSetChanged")
+        fun setData(newData: ArrayList<CommentItem>) {
+            itemList = newData
+            notifyDataSetChanged()
+        }
     }
-}
