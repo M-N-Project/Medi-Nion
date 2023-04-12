e3da2cb3f806f4651c113623eaa493caee6950ba
Gabin Nam <cabin4nam@naver.com>
2023-04-12 16:27:06 +0900

    비즈니스 사진 뷰 수정

--- app/src/main/java/com/example/medi_nion/BusinessManageRecyclerAdapter.kt
+++ app/src/main/java/com/example/medi_nion/BusinessManageRecyclerAdapter.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewOutlineProvider
+import android.widget.HorizontalScrollView
 import android.widget.ImageView
 import android.widget.Scroller
 import android.widget.TextView
@@ -51,9 +52,8 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
         private var view: View = v
 
 
-
         fun bind(listener: View.OnClickListener, item: BusinessBoardItem) {
-             //뒤는 item class 변수명을 입력하면 된다,,,
+            //뒤는 item class 변수명을 입력하면 된다,,,
 
             setViewMore(view.content, view.viewMore)
 
@@ -61,12 +61,17 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
             view.time.text = item.time
             view.content.text = item.content
 
-
+            view.findViewById<HorizontalScrollView>(R.id.businessMG_Img).visibility  = View.GONE
             view.profileImg2.setImageResource(R.drawable.logo)
 
-            if(item.profileImg.length >= 5){
-                if(item.profileImg.substring((item.profileImg).length-4, (item.profileImg).length) == ".jpg"){
-                    val imgUrl = "http://seonho.dothome.co.kr/images/businessProfile/${item.profileImg}"
+            if (item.profileImg.length >= 5) {
+                if (item.profileImg.substring(
+                        (item.profileImg).length - 4,
+                        (item.profileImg).length
+                    ) == ".jpg"
+                ) {
+                    val imgUrl =
+                        "http://seonho.dothome.co.kr/images/businessProfile/${item.profileImg}"
                     val task = ImageLoadTask(imgUrl, view.profileImg2)
                     task.execute()
                 }
@@ -83,25 +88,26 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
                     v: View,
                     data: BusinessPostImgItem,
                     pos: Int
-                ){
+                ) {
                     Log.d("click", pos.toString())
 
                 }
             })
 
-            if(item.image1 != "null"){
+            if (item.image1 != "") {
                 Log.d("imgtiem", item.image1)
                 val imgUrl = "http://seonho.dothome.co.kr/images/businessPost/${item.image1}"
                 view.businessMG_Img.visibility = View.VISIBLE
                 val imgItem = BusinessPostImgItem(item.post_num, item.id, imgUrl)
 
                 imgItems.add(imgItem)
-                view.BusinessBoardImgRecyclerView.layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL,false)
+                view.BusinessBoardImgRecyclerView.layoutManager =
+                    LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
 
                 BusinessImgAdapter = BusinessPostImgRecyclerAdapter(imgItems)
                 view.BusinessBoardImgRecyclerView.adapter = BusinessImgAdapter
             }
-            if(item.image2 != "null"){
+            if (item.image2 != "") {
                 Log.d("imgtiem", item.image2)
                 val imgUrl = "http://seonho.dothome.co.kr/images/businessPost/${item.image2}"
                 view.businessMG_Img.visibility = View.VISIBLE
@@ -111,7 +117,7 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
                 BusinessImgAdapter = BusinessPostImgRecyclerAdapter(imgItems)
                 view.BusinessBoardImgRecyclerView.adapter = BusinessImgAdapter
             }
-            if(item.image3 != ""){
+            if (item.image3 != "") {
                 val imgUrl = "http://seonho.dothome.co.kr/images/businessPost/${item.image3}"
                 view.businessMG_Img.visibility = View.VISIBLE
                 val imgItem = BusinessPostImgItem(item.post_num, item.id, imgUrl)
@@ -120,7 +126,7 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
                 BusinessImgAdapter = BusinessPostImgRecyclerAdapter(imgItems)
                 view.BusinessBoardImgRecyclerView.adapter = BusinessImgAdapter
             }
-            if(item.image4 != ""){
+            if (item.image4 != "") {
                 val imgUrl = "http://seonho.dothome.co.kr/images/businessPost/${item.image4}"
                 view.businessMG_Img.visibility = View.VISIBLE
                 val imgItem = BusinessPostImgItem(item.post_num, item.id, imgUrl)
@@ -129,7 +135,7 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
                 BusinessImgAdapter = BusinessPostImgRecyclerAdapter(imgItems)
                 view.BusinessBoardImgRecyclerView.adapter = BusinessImgAdapter
             }
-            if(item.image5 != ""){
+            if (item.image5 != "") {
                 val imgUrl = "http://seonho.dothome.co.kr/images/businessPost/${item.image5}"
                 view.businessMG_Img.visibility = View.VISIBLE
                 val imgItem = BusinessPostImgItem(item.post_num, item.id, imgUrl)
@@ -143,15 +149,14 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
             view.BusinessBoardImgRecyclerView.adapter = BusinessImgAdapter
 
 
-
 //            view.scrap_btn.text = item.scrap.toString()
 //            view.scrap_btn2.text = item.heart.toString()
 
         }
 
-        private fun setViewMore(contentTextView: TextView, viewMoreTextView: TextView){
+        private fun setViewMore(contentTextView: TextView, viewMoreTextView: TextView) {
             // getEllipsisCount()을 통한 더보기 표시 및 구현
-            contentTextView.post{
+            contentTextView.post {
                 val lineCount = contentTextView.layout.lineCount
                 if (lineCount > 0) {
                     if (contentTextView.layout.getEllipsisCount(lineCount - 1) > 0) {
@@ -160,11 +165,10 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
 
                         // 더보기 클릭 이벤트
                         viewMoreTextView.setOnClickListener {
-                            if(viewMoreTextView.text == "더보기"){
+                            if (viewMoreTextView.text == "더보기") {
                                 contentTextView.maxLines = Int.MAX_VALUE
                                 viewMoreTextView.text = "간략히 보기"
-                            }
-                            else{
+                            } else {
                                 contentTextView.maxLines = Int.MAX_VALUE
                                 viewMoreTextView.text = "더보기"
                             }
@@ -176,7 +180,7 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
             }
         }
 
-        fun roundAll(iv: ImageView, curveRadius : Float)  : ImageView {
+        fun roundAll(iv: ImageView, curveRadius: Float): ImageView {
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 
@@ -197,7 +201,7 @@ class BusinessManageRecyclerAdapter(private val items: ArrayList<BusinessBoardIt
         fun StringToBitmaps(image: String?): Bitmap? {
             try {
                 val encodeByte = Base64.decode(image, Base64.DEFAULT)
-                val bitmap : Bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
+                val bitmap: Bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
                 return bitmap
             } catch (e: Exception) {
                 e.message
