a53158e7b7610a19f239523a19e52f41540b8bf9
5lft <zara0140@naver.com>
2023-05-03 01:40:44 +0900

    학회및세미나 완성 ㅜㅜ

--- app/src/main/java/com/example/medi_nion/MedicalSeminar.kt
+++ app/src/main/java/com/example/medi_nion/MedicalSeminar.kt
@@ -1,21 +1,24 @@
 package com.example.medi_nion
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.medical_seminar_recycler.*
 import kotlinx.coroutines.*
 import org.jsoup.Jsoup
-import java.util.concurrent.Executors
 
 class MedicalSeminar : AppCompatActivity() {
 
     data class MediSeminar (
         val saminar_siteUrl: String,
         val seminar_title: String,
-        val seminar_time_date: String
+        val seminar_time_date: String,
+        val seminar_location: String
     )
 
     val items : ArrayList<MediSeminar> = arrayListOf()
@@ -23,9 +26,18 @@ class MedicalSeminar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.medical_seminar_recycler)
-        Log.d("haha1", "sd")
 
+        asan()
+        samsung()
         kosmi() //대한의료정보학회함수 넣어놓음(이거만 완벽하게 된다 주아야 ㅜㅜ)
+        khidi()
+
+        val kimes = findViewById<LinearLayout>(R.id.ms_kimes)
+        kimes.setOnClickListener {
+            val openUrl = Intent(Intent.ACTION_VIEW)
+            openUrl.data = Uri.parse("https://kimes.kr/kor/")
+            startActivity(openUrl)
+        }
     }
 
 
@@ -37,34 +49,33 @@ class MedicalSeminar : AppCompatActivity() {
     @OptIn(DelicateCoroutinesApi::class)
     private fun asan() {       //아산병원 웹크롤링 함수(아산 클릭시 넘어가는거 onCreate에 추가하기)
         GlobalScope.launch(Dispatchers.IO) {
+
             val url = "https://www.amc.seoul.kr/asan/academy/event/eventList.do"
-            var base_url = "https://www.amc.seoul.kr/asan/academy/event/eventList.do"
+            var base_url = "https://www.amc.seoul.kr/asan/academy/event/eventDetail.do"
 
             val doc = Jsoup.connect(url)
                 .timeout(5000) // Set a timeout value (in milliseconds)
                 .get()
 
-            val today = doc.select("div.dayListWrap ul.dayListBox")
+            val today = doc.select("div.dayListWrap ul.dayListBox li.dayListBoxLeft")
             val title = doc.title()
-            println("haha2: ${today.size}")
 
             val items = mutableListOf<MediSeminar>()
 
             today.forEach { item ->
-                val item_link = base_url + item.select("a[onclick*=fnDetail('1379')]").attr("href")
+                val item_link = base_url + "?eventId=" + item.select("div.dayListTitle a").attr("onclick").replace("[^0-9]".toRegex(), "")
                 val item_title = item.select("div.dayListTitle").text()
-                val item_time = item.select("div.dayListTitle2").text()
+                var item_time = item.select("div.dayListTitle2").text()
+                val item_location = item_time.split("행사일 : ")[0]
+                item_time = "일시 : " + item_time.split("행사일 : ")[1]
 
-                println("haha3: $item_link")
-
-                items.add(MediSeminar(item_link, item_title, item_time))
+                items.add(MediSeminar(item_link, item_title, item_time, item_location))
             }
 
             launch(Dispatchers.Main) {
-                seminarrecyclerview.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
+                asanRecycler.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
                 val adapter = MedicalSeminarAdapter(items as ArrayList<MediSeminar>, this@MedicalSeminar)
-                seminarrecyclerview.adapter = adapter
-                println("haha4: ${items.size}")
+                asanRecycler.adapter = adapter
             }
         }.start()
     }
@@ -83,25 +94,23 @@ class MedicalSeminar : AppCompatActivity() {
 
             val today = doc.select("div.card-content-textarea")
             val title = doc.title()
-            println("haha2: ${today.size}")
 
             val items = mutableListOf<MediSeminar>()
 
             today.forEach { item ->
-                val item_link = item.select("a").attr("href")
+                val item_link = "https://www.samsunghospital.com" + item.select("a").attr("href")
                 val item_title = item.select("a.card-content-title").text()
-                val item_time = item.select("div.card-contet-text.card-conference-content ul").text()
+                var item_time = item.select("div.card-content-text.card-conference-content ul li").text()
+                val item_location = "장소 : " + item_time.split("장소 : ")[1]
+                item_time = "일시 : " + item_time.split("장소 : ")[0].split("시간 : ")[1]
 
-                println("haha3: $item_link")
-
-                items.add(MediSeminar(item_link, item_title, item_time))
+                items.add(MediSeminar(item_link, item_title, item_time, item_location))
             }
 
             launch(Dispatchers.Main) {
-                seminarrecyclerview.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
+                samsungRecycler.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
                 val adapter = MedicalSeminarAdapter(items as ArrayList<MediSeminar>, this@MedicalSeminar)
-                seminarrecyclerview.adapter = adapter
-                println("haha4: ${items.size}")
+                samsungRecycler.adapter = adapter
             }
         }.start()
     }
@@ -118,27 +127,28 @@ class MedicalSeminar : AppCompatActivity() {
                 .timeout(5000) // Set a timeout value (in milliseconds)
                 .get()
 
-            val today = doc.select("li.list_li_box a.bo_subject")
+            val today = doc.select("li.list_li_box")
             val title = doc.title()
-            println("haha2: ${today.size}")
 
             val items = mutableListOf<MediSeminar>()
 
             today.forEach { item ->
                 val item_link =  item.select("a").attr("href")
-                val item_title = item.select("a.bo_subject").text()
-                val item_time = item.select("div.bo_cnt_place").text()
+                var item_title = item.select("div.bo_cnt.bo_width40 a.bo_subject").text()
+                item_title = item_title.split("장소")[0]
+                val item_location = item.select("div.bo_cnt_place").text()
 
-                println("haha3: $item_link")
+                val item_month = item.select("div.bo_width10 div.bo_month").text()
+                val item_day = item.select("div.bo_width20 div.bo_day").text()
+                val item_time = "일시 : " + item_month + " " + item_day
 
-                items.add(MediSeminar(item_link, item_title, item_time))
+                items.add(MediSeminar(item_link, item_title, item_time, item_location))
             }
 
             launch(Dispatchers.Main) {
-                seminarrecyclerview.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
+                daehanRecycler.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
                 val adapter = MedicalSeminarAdapter(items as ArrayList<MediSeminar>, this@MedicalSeminar)
-                seminarrecyclerview.adapter = adapter
-                println("haha4: ${items.size}")
+                daehanRecycler.adapter = adapter
             }
         }.start()
     }
@@ -157,25 +167,22 @@ class MedicalSeminar : AppCompatActivity() {
 
             val today = doc.select("td.ellipsis")
             val title = doc.title()
-            println("haha2: ${today.size}")
 
             val items = mutableListOf<MediSeminar>()
 
             today.forEach { item ->
-                val item_link = item.select("a").attr("href")
-                val item_title = item.select("a.title").text()
-                val item_time = item.select("td").text()
+                val item_link = "https://www.khidi.or.kr/" + item.select("a").attr("href")
+                val item_time = ""
+                val item_location=""
+                val item_title = item.select("td").text()
 
-                println("haha3: $item_link")
-
-                items.add(MediSeminar(item_title, item_time, item_link))
+                items.add(MediSeminar(item_link, item_title, item_time, item_location))
             }
 
             launch(Dispatchers.Main) {
-                seminarrecyclerview.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
+                khidiRecycler.layoutManager = LinearLayoutManager(this@MedicalSeminar, RecyclerView.VERTICAL, false)
                 val adapter = MedicalSeminarAdapter(items as ArrayList<MediSeminar>, this@MedicalSeminar)
-                seminarrecyclerview.adapter = adapter
-                println("haha4: ${items.size}")
+                khidiRecycler.adapter = adapter
             }
         }.start()
 
--- app/src/main/java/com/example/medi_nion/MedicalSeminarAdapter.kt
+++ app/src/main/java/com/example/medi_nion/MedicalSeminarAdapter.kt
@@ -3,6 +3,7 @@ package com.example.medi_nion
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -26,8 +27,16 @@ class MedicalSeminarAdapter(val items: ArrayList<MedicalSeminar.MediSeminar>, co
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.view.textView_title?.text = items[position].seminar_title // Title
-        holder.view.textView_date.text = items[position].seminar_time_date // Time date
+        holder.view.ms_title?.text = items[position].seminar_title // Title
+        holder.view.ms_time.text = items[position].seminar_time_date // Time date
+        holder.view.ms_loca.text = items[position].seminar_location // Location
+
+        if(items[position].seminar_time_date.equals("")) {
+            holder.view.ms_time.visibility = View.GONE
+        }
+        if(items[position].seminar_location.equals("")){
+            holder.view.ms_loca.visibility = View.GONE
+        }
 
         holder.itemView.setOnClickListener {
             val openUrl = Intent(Intent.ACTION_VIEW)
