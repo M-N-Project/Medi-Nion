e32a1b389700c6d490fad09c0699ff5f3e314f8d
장선호 <sunho3456@naver.com>
2023-05-15 14:51:34 +0900

    boardDetail xml 수정중

--- app/src/main/java/com/example/medi_nion/Board.kt
+++ app/src/main/java/com/example/medi_nion/Board.kt
@@ -255,6 +255,7 @@ class Board : AppCompatActivity() {
                                         intent.putExtra("board", board)
                                         intent.putExtra("num", data.num)
                                         intent.putExtra("id", id)
+                                        intent.putExtra("commentCnt", data.comment)
                                         intent.putExtra("device_id", device_id)
                                         intent.putExtra("nickname", nickname)
                                         intent.putExtra("writerId", detailId)
@@ -265,7 +266,7 @@ class Board : AppCompatActivity() {
                                         intent.putExtra("userType", userType)
                                         intent.putExtra("userDept", userDept)
                                         intent.putExtra("userMedal", userMedal)
-                                        intent.putExtra("commentCnt", detailCommentCnt)
+//                                        intent.putExtra("commentCnt", detailCommentCnt)
                                         startActivity(intent)
                                     }
 
